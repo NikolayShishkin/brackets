a44c150396b554824c92a14f3f48d2bf1d7c4b40
NikolayShishkin <r58korin@gmail.com>
2019-10-08 11:14:52 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stringLength = str.length;
+  if (stringLength % 2 !== 0) {
+      return false;
+  }
+  while(stringLength > 0){
+
+  }
+  return true;
 }
